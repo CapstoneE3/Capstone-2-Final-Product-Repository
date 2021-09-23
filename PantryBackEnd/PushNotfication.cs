f23c04692bb1712d32c88fad0c8b7d7d0592a761
Adenan Hasan Shaleh <Adenan_2000@hotmail.com>
2021-09-23 21:22:43 +0800

    Commit to Notification

---
+++ PantryBackEnd/PushNotfication.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+namespace PantryBackEnd
+{
+    public class PushNotfication :BackgroundService
+    {
+        private readonly ILogger<PushNotfication> _logger;
+
+        public PushNotfication(ILogger<PushNotfication> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            Timer setSchdule = new Timer()
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
