5bd1598633d5b3179960c187a7c15dcc938f79cf
Adenan Hasan Shaleh <Adenan_2000@hotmail.com>
2021-09-25 11:11:31 +0800

    Refactor getInventory

--- PantryBackEnd/Repositories/InventoryRepo.cs
+++ PantryBackEnd/Repositories/InventoryRepo.cs
@@ -19,28 +19,63 @@ namespace PantryBackEnd.Repositories
             context.InventoryLists.Add(product);
             context.SaveChanges();
         }
-        public void updateItem(InventoryList product)
+        public void updateItem()
         {
-            context.InventoryLists.Update(product);
             context.SaveChanges();
         }
         public Dictionary<string, object> GetInventoryList(Guid acc_id)
         {
+            int listCount = 0;
+            List<FrontEndInventoryFormat> formats;
             Dictionary<string, object> invList = new Dictionary<string, object>();
             string[] category = {"Kids & Lunch Box","Entertaining At Home","Bakery","Fruit & Vegetables",
                                 "Meat & Seafood","From The Deli","Dairy, Eggs & Meals","Conveniece Meals",
                                 "Pantry","Frozen","Drinks","International Foods","Household","Health & Beauty",
-                                "Baby","Pet","Liquor","Tobacco"};
+                                "Baby","Pet","Liquor","Tobacco","Sugar & Sweeteners"};
             foreach (string i in category)
             {
-                Product prods = context.Products.Where(a => a.Category.Equals(i)).Include(b => b.InventoryLists.Where(c => c.AccId == acc_id)).FirstOrDefault();
-                try
+                List<Product> prods = context.Products.Where(a => a.Category.Equals(i)).Include(b => b.InventoryLists.Where(c => c.AccId == acc_id)).ToList();
+                if (prods.Count > 0)
                 {
-                    invList.Add(i, prods.InventoryLists);
+                    formats = new List<FrontEndInventoryFormat>();
+                    foreach (Product c in prods)
+                    {
+
+                        if (c.InventoryLists.Count != 0)
+                        {
+                            listCount = 0;
+                            FrontEndInventoryFormat b = new FrontEndInventoryFormat();
+                            foreach (InventoryList a in c.InventoryLists)
+                            {
+                                ExpDateAndPrice eap = new ExpDateAndPrice
+                                {
+                                    expDate = a.ExpDate,
+                                    count = (int)a.Count
+                                };
+                                if (listCount == 0)
+                                {
+                                    b.itemID = a.ItemId;
+                                    b.name = c.Name;
+                                    b.price = (decimal)c.Price;
+                                    b.quantity = c.Quantity;
+                                    b.Expiry_Count.Add(eap);
+                                }
+                                else
+                                {
+                                    b.Expiry_Count.Add(eap);
+                                }
+                                listCount++;
+
+
+                            }
+                            formats.Add(b);
+                        }
+                    }
+                    invList.Add(i, formats);
                 }
-                catch (NullReferenceException)
+                else
                 {
-                    invList.Add(i, new List<InventoryList>());
+                    invList.Add(i, new List<FrontEndInventoryFormat>());
                 }
             }
             return invList;
