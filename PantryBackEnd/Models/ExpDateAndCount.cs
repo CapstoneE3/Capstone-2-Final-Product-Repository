6ee9b0afbd02e7d67c21c7ad84a1d6986a04150b
Adenan Hasan Shaleh <Adenan_2000@hotmail.com>
2021-09-25 11:08:46 +0800

    Inventory Front end Format

---
+++ PantryBackEnd/Models/ExpDateAndCount.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PantryBackEnd.Models
+{
+    public class ExpDateAndPrice
+    {
+        public DateTime expDate;
+        public int count;
+    }
+}
