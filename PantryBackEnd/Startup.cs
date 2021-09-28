4cd15028abfc9a55ca349ca62f677d95ae6f38c5
Adenan Hasan Shaleh <Adenan_2000@hotmail.com>
2021-09-28 22:07:01 +0800

    Update Startup.cs

--- PantryBackEnd/Startup.cs
+++ PantryBackEnd/Startup.cs
@@ -59,9 +59,10 @@ namespace PantryBackEnd
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PantryBackEnd v1"));
+                
             }
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PantryBackEnd v1"));
 
             app.UseHttpsRedirection();
             app.UseCors(policy => policy
