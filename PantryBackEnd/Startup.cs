891ae8ddbdcfe32cdd83d841d37003bded2a630d
Adenan Hasan Shaleh <Adenan_2000@hotmail.com>
2021-10-12 13:10:51 +0800

    options to add front end url

--- PantryBackEnd/Startup.cs
+++ PantryBackEnd/Startup.cs
@@ -67,7 +67,7 @@ namespace PantryBackEnd
             app.UseCors(policy => policy
                 .AllowAnyHeader()
                 .AllowAnyMethod()
-                .WithOrigins("https://localhost:5001")
+                .WithOrigins(new[] { "" })
                 .AllowCredentials());
             app.UseRouting();
 
